18c75be12b41247a29da1d75c2f265967d09e9b3
Dario Di Pasquale <9026586+dariodip@users.noreply.github.com>
2021-10-16 12:37:56 +0200

    first commit

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :rumbl,
+  ecto_repos: [Rumbl.Repo]
+
+# Configures the endpoint
+config :rumbl, RumblWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "SgnaVMp0gqyaaM6g5ot6JxLQ4RRboeD1KHMHBH96LNb5Z4RPAErs3WNBcwBD6pRE",
+  render_errors: [view: RumblWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Rumbl.PubSub,
+  live_view: [signing_salt: "KpHsP3jA"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
---
+++ scripts/start_db.sh
@@ -0,0 +1,1 @@
+docker run -d --name phoenix-psql -e POSTGRES_PASSWORD=Phoenix1234 -v /Users/dario/phoenix-postgres-data/:/var/lib/postgresql/data -p 5432:5432 postgres
