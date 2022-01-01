f598b9fd883e03f1ac6331eb5ddc2c771adbde38
dariodip <darionovantadue@gmail.com>
2022-01-01 10:28:37 +0000

    Add check alias

--- mix.exs
+++ mix.exs
@@ -67,7 +67,8 @@ defmodule Rumbl.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      check: ["dialyzer", "credo", "format --check-formatted"]
     ]
   end
 end
